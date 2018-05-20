89fab8ce0f9bcd222ca6eb080d04e2a125d56c40
Darren Hay <darrenhay1994@gmail.com>
2018-05-20 22:26:52 +0100

    creating national insurance function

--- calc.js
+++ calc.js
@@ -8,13 +8,35 @@ const reducePersonalAllowance = (salary, allowance) => {
     return salary
 }
 
+const calculateNationalInsurance = (salary, nationalInsuranceBrackets) => {
+    let nationalInsuranceBracketsUsed = nationalInsuranceBrackets.filter(brackets => {
+        if(salary >= brackets.bracketMin) {
+            brackets.taxableNIAmount = {}
+            brackets.taxableNIAmount.amountOfNIInThisBracket = (salary - brackets.bracketMin) >= brackets.bracketMax ? brackets.bracketMax : salary - brackets.bracketMin 
+            brackets.taxableNIAmount.amountNITaxedInThisBracket =  brackets.taxableNIAmount.amountOfNIInThisBracket * (brackets.NIPercentage / 100)
+            return brackets
+        }
+    })
+    
+    let totalNationalInsuranceAmount = nationalInsuranceBracketsUsed.reduce((accumulator, NIAmount) => {
+        let NIAmountForBracket = (NIAmount.taxableNIAmount ? NIAmount.taxableNIAmount.amountNITaxedInThisBracket : 0)
+        return accumulator + NIAmountForBracket
+    }, 0)
+
+    let nationalInsuranceValues = {
+        "bracketsUsed": nationalInsuranceBracketsUsed,
+        "totalNIAmount": totalNationalInsuranceAmount
+    }
+
+    return nationalInsuranceValues
+    
+}
+
 const calcTax = (taxBrackets, salary, taxYear) => {
     let currentTaxYearValues = taxBrackets.find(taxYears => taxYears.taxYear === taxYear)
-    
+
     // total amount of taxable income after personal allowance
     let taxableIncome = salary - (salary >= 100000 ? reducePersonalAllowance(salary, currentTaxYearValues.allowance) : currentTaxYearValues.allowance)
-    
-    console.log(taxableIncome)
     
     let bracketsUsed = currentTaxYearValues.taxBrackets.filter(brackets => {
         if(taxableIncome >= brackets.bracketMin) {
@@ -25,12 +47,16 @@ const calcTax = (taxBrackets, salary, taxYear) => {
         }
     })
 
-    console.log(bracketsUsed)
+    let nationalInsuranceAmount = calculateNationalInsurance(salary, currentTaxYearValues.nationalInsuranceBrackets) 
     
     console.log(salary)
-    
-    console.log('logging')
-    
+    // console.log(allowance)
+    console.log(taxableIncome)
+    // console.log(totalIncomeTaxAmount)
+    // console.log(bracketBreakdownOfTown)
+    console.log(nationalInsuranceAmount)
+    // console.log(totalTaxedNIAndIncome)
+    // console.log(netWage)
 }
 
 let taxBrackets = [
@@ -53,11 +79,31 @@ let taxBrackets = [
                 "bracketMin": 150000,
                 "bracketMax": 9999999999
             }
+        ],
+        "nationalInsuranceBrackets": [
+            {
+                "name": "Less than £8,424",
+                "bracketMin": 0,
+                "bracketMax": 8424,
+                "NIPercentage": 0
+            },
+            {
+                "name": "£8,424 - £46,350",
+                "bracketMin": 8424,
+                "bracketMax": 46350,
+                "NIPercentage": 12
+            },
+            {
+                "name": "More than £46,350",
+                "bracketMin": 46350,
+                "bracketMax": 9999999999,
+                "NIPercentage": 2
+            }
         ]
     }
 ]
 
-let salary = 160000
+let salary = 27000
 let taxYear = "2018/2019"
 
 calcTax(taxBrackets, salary, taxYear)
